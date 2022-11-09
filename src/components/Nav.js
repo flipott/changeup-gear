9fdc2710b9d75b2236a48c414b9e1ff36b40e33f
flipott <ottphillipa@gmail.com>
2022-11-08 19:03:25 -0500

    Change a tags to react-router-dom links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "../css/Nav.css"
+import { Link } from "react-router-dom";
 
 export default function Nav() {
     return (
         <div className="nav">
-            <h1><a href="">Changeup Gear</a></h1>
+            <h1><Link to="/">Changeup Gear</Link></h1>
             <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Shop</a></li>
-                <li><a href="">Cart</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/shop">Shop</Link></li>
+                <li><Link to="/cart">Cart</Link></li>
             </ul>
         </div>
     )
