2d0a66cf77faf073d31e9036cdb8d4827a4659e6
flipott <ottphillipa@gmail.com>
2022-11-08 21:45:18 -0500

    Create Data component to store item information

---
+++ src/components/Data.js
@@ -0,0 +1,42 @@
+const Data = {
+    items: [
+        {
+            title: "Louisville Slugger 32 Bat",
+            category: "bat",
+            price: "$49.99",
+            image: "bat_1.jpg"
+        },
+        {
+            title: "Louisville Slugger 32 Bat",
+            category: "bat",
+            price: "$49.99",
+            image: "bat_1.jpg"
+        },
+        {
+            title: "Louisville Slugger 32 Bat",
+            category: "bat",
+            price: "$49.99",
+            image: "bat_1.jpg"
+        },
+        {
+            title: "Louisville Slugger 32 Bat",
+            category: "bat",
+            price: "$49.99",
+            image: "bat_1.jpg"
+        },
+        {
+            title: "Louisville Slugger 32 Bat",
+            category: "bat",
+            price: "$49.99",
+            image: "bat_1.jpg"
+        },
+        {
+            title: "Louisville Slugger 32 Bat",
+            category: "bat",
+            price: "$49.99",
+            image: "bat_1.jpg"
+        }
+    ]
+}
+
+export default Data;
