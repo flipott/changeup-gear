8b328989401edd04e909899010ec114de8b25d5a
flipott <ottphillipa@gmail.com>
2022-11-09 19:18:59 -0500

    Implement ability to filter items by category

--- src/Shop.js
+++ src/Shop.js
@@ -5,34 +5,61 @@ import Card from "./components/Card";
 import "./css/Shop.css";
 import Data from "./components/Data";
 
-const itemArray = Data.items;
-const listItems = itemArray.map((item) =>
-    <Card 
-    title={item.title} 
-    category={item.category}
-    price={item.price}
-    image={item.image}
-    />
-);
 
 function Shop() {
+
+    const itemArray = Data.items;
+    const [items, setItems] = React.useState(itemArray);
+    const [currentSelection, setCurrentSelection] = React.useState("All Products");
+
+
+    const listItems = items.map((item) =>
+        <Card 
+        title={item.title} 
+        category={item.category}
+        price={item.price}
+        image={item.image}
+        key={item.image}
+        />
+    );
+
+    const filterItems = (category, event) => {
+        setCurrentSelection(event.textContent);
+
+        const categoryList = document.querySelector(".list-parent");
+        for (let i = 0; i < categoryList.children.length; i++) {
+            if (categoryList.children[i].classList.contains("bold")) {
+                categoryList.children[i].classList.remove("bold");
+            };
+        };
+
+        event.classList.add("bold");
+
+        if (category === "all") {
+            setItems(itemArray);
+        } else {
+            setItems(itemArray.filter((item) => item.category === category));
+        }
+
+    }
+
     return (
         <div className="App">
             <Nav />
             <div className="shop--main">
                 <div className="sidebar">
-                    <ul>
-                        <li><a href="">All Products</a></li>
-                        <li><a href="">Bats</a></li>
-                        <li><a href="">Gloves</a></li>
-                        <li><a href="">Balls</a></li>
-                        <li><a href="">Apparel</a></li>
-                        <li><a href="">Training Gear</a></li>
-                        <li><a href="">Accessories</a></li>
+                    <ul className="list-parent">
+                        <li onClick={(e) => filterItems("all", e.target)}>All Products</li>
+                        <li onClick={(e) => filterItems("bat", e.target)}>Bats</li>
+                        <li onClick={(e) => filterItems("glove", e.target)}>Gloves</li>
+                        <li onClick={(e) => filterItems("ball", e.target)}>Balls</li>
+                        <li onClick={(e) => filterItems("apparel", e.target)}>Apparel</li>
+                        <li onClick={(e) => filterItems("training", e.target)}>Training Gear</li>
+                        <li onClick={(e) => filterItems("accessory", e.target)}>Accessories</li>
                     </ul>
                 </div>
                 <div className="item-section">
-                    <div className="item-title">Viewing All Products</div>
+                    <div className="item-title">Viewing {currentSelection}</div>
                     <div className="items">
                         {listItems}
                     </div>
